ed193c818411facf85a4a8be96a75bf481ac9bd0
Amalia Jamaludin <amaliajamaludin12@gmail.com>
2024-12-03 10:15:58 -0500

    Add link to login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,7 +4,6 @@ import StyledButton from '../components/StyledButton';
 import TextInputBox from '../components/TextInputBox';
 import { useNavigate } from 'react-router-dom';
 import logo from '../assets/CityWatch.png';
-import { Link } from 'react-router-dom';
 import './styles/LoginPage.css';
 
 const LoginPage = () => {
